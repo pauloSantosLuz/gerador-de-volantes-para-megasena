ac95269da708daa431d281544232c965cf86e304
Paulo Henrique Luz <paulohenrique.luz@hotmail.com>
2019-07-09 03:50:40 -0300

    tela PreAnalise todos as possibilidades implementadas

--- src/app/pre-analise-sorteio/pre-analise-sorteio.module.ts
+++ src/app/pre-analise-sorteio/pre-analise-sorteio.module.ts
@@ -8,6 +8,9 @@ import { IonicModule } from '@ionic/angular';
 import { PreAnaliseSorteioPage } from './pre-analise-sorteio.page';
 import { JogoControllerService } from '../jogo-controller.service';
 import { NgxMaskIonicModule } from 'ngx-mask-ionic';
+import { FileChooser } from '@ionic-native/file-chooser/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 const routes: Routes = [
   {
@@ -25,6 +28,9 @@ const routes: Routes = [
     RouterModule.forChild(routes)
   ],
   providers: [
+    File,
+    FileChooser,
+    FilePath
 //    JogoControllerService
   ],
   declarations: [PreAnaliseSorteioPage]
